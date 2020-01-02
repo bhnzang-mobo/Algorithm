ec5d03af47321d2559faa7a00dc09ac8ebf7d882
MOBO <bhnzang9608@gmail.com>
2020-01-02 17:59:14 +0900

    Time out error occurs only for java T.T

--- bj/src/solution/q2447.java
+++ bj/src/solution/q2447.java
@@ -1,38 +1,34 @@
 package solution;
-
+//2447문제는 자바 한정 시간초과로 패스. 똑같은 알고리즘인데 자바는 안되고 C는 된다. 자바라서 당함 시발.
 import java.util.*;
 
 public class q2447 {
 	char pane[][];
 	public void stars(int x,int y, int r) {
 		if(3<=r) {
-			stars(x,y,r/3);
-			stars(x+r/3,y,r/3);
-			stars(x+2*r/3,y,r/3);
-			
-			stars(x,y+r/3,r/3);
-			stars(x+2*r/3,y+r/3,r/3);
-			
-			stars(x,y+2*r/3,r/3);
-			stars(x+r/3,y+2*r/3,r/3);
-			stars(x+2*r/3,y+2*r/3,r/3);
-			
-			//remove center
-			for(int i = y+r/3 ; i < y+2*r/3; i ++) {
-				for(int j = x+r/3 ; j < x+2*r/3 ; j ++) {
-						this.pane[j-1][i-1]=' ';
+			for(int j = 0 ; j < 3 ; j ++) {
+				for(int i = 0 ; i < 3 ; i ++) {
+					if(i==1 && j ==1) {
+						continue;
+					}
+					stars(x+(i*r/3),y+(j*r/3),r/3);
 				}
 			}
+			
+			//remove center
+		}
+		else {
+			this.pane[x-1][y-1]='*';
 		}
 		
 	}
-	public static void main(String args[]) {
+	public void main(String args[]) {
 		q2447 qst = new q2447();
 		Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
 		qst.pane=new char[a][a];
 		for(int i = 0 ; i < a ; i ++) {
-			Arrays.fill(qst.pane[i],'*');
+			Arrays.fill(qst.pane[i],' ');
 		}
 		
 		
