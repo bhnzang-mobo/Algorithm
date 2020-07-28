1becb5e7935cda544075b007ade086444050fd94
MOBO <bhnzang9608@gmail.com>
2020-07-28 16:12:12 +0900

    BJ solved 20_07_28

--- 1436/main.c
+++ 1436/main.c
@@ -1,143 +1,23 @@
-#include<stdio.h>
-
-int  process(int index, int number)
-{
-
-
-	int tmp;
-	int count;
-	int i;
-	int ret;
-
-	
-	tmp = number;
-	count = 0; 
-	while (tmp != 0)
-	{
-		if (tmp % 10 == 6)
-		{
-			count++;
-			if (count == 3)
-			{
-				int base;
-				
-				base = 1;
-
-				for (i = 0; i < index; i++)
-					base = base * 10;
-
-				return base;
-			}
-		}
-		else
-		{
-			break;
-		}
-
-		tmp = tmp / 10;
-	}
-	if (index == 3 && count == 0)
-		return 1;
-	if (index == 3 && count == 1)
-		return 10;
-	if (index == 3 && count == 2)
-		return 100;
-
-
-	ret = 0;
-
-	for (i = 0; i < 10; i++)
-	{
-		ret += process(index - 1, number * 10 + i);
-	}
-
-
-
-	return ret;
-
-}
-
-
-int find(int index, int number, int remain)
-{
-	int sum;
-	int ret;
-	int i;
-	int base;
-	int tmp;
-	int count;
-
-	tmp = number;
-	count = 0;
-
-
-
-	while (tmp != 0)
-	{
-		if (tmp % 10 == 6)
-		{
-			count++;
-			if (count == 3)
-			{
-				int base;
-
-				base = 1;
-
-				for (i = 0; i < index; i++)
-					base = base * 10;
-
-				return number*base + remain-1;
-			}
-		}
-		else
-		{
-			break;
-		}
-
-		tmp = tmp / 10;
-	}
-
-	if (index == 3 && count == 0)
-	{
-		return number * 1000 + 666;
-	}
-	if (index == 3 && count == 1)
-	{
-		return number * 1000 + 660 + remain-1;
-	}
-	if (index == 3 && count == 2)
-	{
-		return number * 1000 + 600 + remain-1;
-
-	}
-
-
-
-	sum = 0;
-
-	for (i = 0; i < 10; i++)
-	{
-		ret = process(index - 1, number * 10 + i);
-
-		if (sum + ret >= remain)
-			break;
-		else
-			sum = sum + ret;
-	}
-	
-	return find(index - 1, number * 10 + i, remain - sum);
-
-}
-
+#include <stdio.h>
 
 int main()
 {
-	int N;
-
+	int N, ch, cnt, n = 1, i = 666;
 	scanf("%d", &N);
-
-	printf("%d\n", find(7, 0, N));
-
+	N--;
+	while (N)
+	{
+		cnt = 1;
+		ch = n;
+		while (ch%10 == 6)
+		{
+			cnt *= 10;
+			ch /= 10;
+		}
+		for (int j = 0; j < cnt && N; j++, N--)
+            i = (n / cnt) * (1000 * cnt) + (666 * cnt) + j;
+		n++;
+	}
+	printf("%d", i);
 	return 0;
-
 }
