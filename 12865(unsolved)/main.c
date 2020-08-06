f7884d2f50d07a592976d8876dcc6d7ff4d57328
MOBO <bhnzang9608@gmail.com>
2020-08-06 16:48:50 +0900

    BJ_solved 20_08_06

--- 12865(unsolved)/main.c
+++ 12865(unsolved)/main.c
@@ -1,73 +1,37 @@
 #include <stdio.h>
 
-int arr[101][3];
-int memo[100001][101];
-int N,K;
+int item_val[101];
+int item_weight[101];
+int dp[101][100001];
+int N,W;
 
-
-int big(int K,int m){//이게 시간복잡도 오지게잡아먹음 거의 n^3?
-    int max = 0;
-    int idx=-1;
-    for(int i = 0 ; i < N; i ++){//가져올 수 있는 K무게의 물건들 중 최대값
-        if(memo[m][i]==0){
-            if(arr[i][0]==K && max<arr[i][1]){
-            max=arr[i][1];
-            idx=i;
-            }
-        }       
-    }
-    return idx; //그 물건의 인덱스값. arr[idx][1] 이 그 가치. -1은 무게에 해당하는 물건이 없다.
-}
-
-void copy(int K,int M){
-    for(int i = 0 ; i < N ; i++){
-        memo[M][i]=memo[K][i];
-    }
-}
-int cal(int K){
-    if(K<=1){
-        int idx = big(1,1);
-        memo[1][idx]=1;
-        memo[1][100]=arr[idx][1];
-        return arr[idx][1];
-    }
-    if(memo[K][100]>0){
-        return memo[K][100];
-    }
-    int flag=0, max=-1;
-    int single = big(K,K);
-    int single_val = single>=0?arr[single][1]:0;
-    int idx_i=-1;
-    for(int i = 1 ; i <=K/2 ; i++){
-        int comp = cal(K-i);
-        int comp_single=big(i,K-i);
-        int comp_single_val=comp_single>=0?arr[comp_single][1]:0;
-        if(comp+comp_single_val>single_val){
-            flag=1;
-            single_val=comp+comp_single_val;
-            idx_i=comp_single;
-            max=i;
+void dpCal(){
+    for(int i = 1 ; i <= W ; i ++){
+        if(i>=item_weight[1]){//weight[i]가 아니다
+            dp[1][i]=item_val[1];//val[i]가 아니다
         }
     }
-    if(flag>=1){
-        copy(K-max,K);
-        if(idx_i>=0){
-            memo[K][idx_i]=1;
-            }
-        memo[K][100]=single_val;
-    }
-    else{
-        memo[K][single]=1;
-        memo[K][100]=single_val;
-    }
-    return single_val;
-}
 
-int main(){
-    scanf("%d %d",&N,&K);
-    for(int i = 0 ; i < N ; i ++){
-        scanf("%d %d",&arr[i][0],&arr[i][1]);
+    for(int i = 2 ; i <= N ; i ++){
+        for(int j = 1 ; j <= W ; j ++){
+            if(j>=item_weight[i]){
+                int comp1 = dp[i-1][j];
+                int comp2 = dp[i-1][j-item_weight[i]]+item_val[i];
+                dp[i][j]=comp1>comp2? comp1 : comp2;
+            }
+            else{
+                dp[i][j]=dp[i-1][j];
+            }
+        }
     }
-    printf("%d",cal(K));
+}
+int main(){
+    scanf("%d %d",&N,&W);
+    for(int i = 1 ; i <= N ; i ++){
+        scanf("%d %d",&item_weight[i],&item_val[i]);
+    }
+    dpCal();
+    printf("%d",dp[N][W]);
+
     return 0;
 }
